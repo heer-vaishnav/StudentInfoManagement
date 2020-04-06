c0009846a29bf756bf772729248224ca37ff8fe6
heer-vaishnav <48050788+heer-vaishnav@users.noreply.github.com>
2020-04-06 22:47:07 +0530

    Add files via upload

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentInfoManagement.Domain;
+using StudentInfoManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentInfoManagement.Controller
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController
+    {
+        public CourseController()
+        {
+            this.CourseDomain = new CourseDomain();
+        }
+        private CourseDomain CourseDomain { get; set; }
+    }
+}
