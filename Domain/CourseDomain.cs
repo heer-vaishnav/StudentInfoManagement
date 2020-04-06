719cfe3ba112cc583826a4606f8309742f854ca4
heer-vaishnav <48050788+heer-vaishnav@users.noreply.github.com>
2020-04-06 22:45:44 +0530

    Add files via upload

---
+++ Domain/CourseDomain.cs
@@ -0,0 +1,26 @@
+﻿using StudentInfoManagement.Domains;
+using StudentInfoManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentInfoManagement.Domain
+{
+    public class CourseDomain : BaseDomain
+    {
+        public List<Course> Get(int CourseId)
+        {
+            var reader = this.GetReader($"select * from Courses ");
+            var courses = new List<Course>();
+            while (reader.Read())
+            {
+                var course = new Course();
+                course.CourseId = reader.GetInt32(1);
+                course.CourseName = reader.GetString(2);
+                courses.Add(course);
+            }
+            return courses;
+        }
+    }
+}
---
+++ Domain/StudentDomain.cs
@@ -0,0 +1,76 @@
+﻿using StudentInfoManagement.Domains;
+using StudentInfoManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentInfoManagement.Domain
+{
+    public class StudentDomain : BaseDomain
+    {
+        //public bool IsLogin(Student student)
+        //{
+        //    var reader = this.GetReader($"select * from Students where Name='{student.StudentName}' and EmailId = '{student.StudentEmailId}'");
+        //    var isLoggedIn = false;
+        //    while (reader.Read())
+        //    {
+        //        isLoggedIn = true;
+        //    }
+        //    return isLoggedIn;
+        //}
+
+        public List<Student> StudentGet()
+        {
+            var reader = this.GetReader($"select * from Students ");
+            var students = new List<Student>();
+            while (reader.Read())
+            {
+                var student = new Student();
+                student.StudentName = reader.GetString(2);
+                student.StudentEmailId = reader.GetString(3);
+                student.StudentMobileNo = reader.GetString(4);
+                student.FeesPaid = reader.GetBoolean(5);
+                student.CourseId = reader.GetInt16(6);
+                students.Add(student);
+            }
+            return students;
+        }
+
+        public void StudentAdd(Student student)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=ADMIN-PC\\SQLEXPRESS;Initial Catalog=StudentInfo;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand($"spInsert", conn);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            conn.Open();
+            cmd.Parameters.AddWithValue("@StudentName", student.StudentName);
+            cmd.Parameters.AddWithValue("@StudentEmail", student.StudentEmailId);
+            cmd.Parameters.AddWithValue("@StudentMobileNo", student.StudentMobileNo);
+            cmd.Parameters.AddWithValue("@FeesPaid", student.FeesPaid);
+            cmd.ExecuteNonQuery();
+        }
+
+        public void StudentUpdate(Student student)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=ADMIN-PC\\SQLEXPRESS;Initial Catalog=StudentInfo;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand($"SpUpdate", conn);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            conn.Open();
+            cmd.Parameters.AddWithValue("@StudentName", student.StudentName);
+            cmd.Parameters.AddWithValue("@StudentEmail", student.StudentEmailId);
+            cmd.Parameters.AddWithValue("@StudentMobileNo", student.StudentMobileNo);
+            cmd.Parameters.AddWithValue("@FeesPaid", student.FeesPaid);
+            cmd.ExecuteNonQuery();
+        }
+
+        public void StudentDelete(Student student)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=ADMIN-PC\\SQLEXPRESS;Initial Catalog=StudentInfo;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand($"spDelete", conn);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            conn.Open();
+            cmd.Parameters.Remove(student.StudentId);
+        }
+    }
+}
